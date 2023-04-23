4b6fdf056641a3f7deba1443839e6e20e98393f2
Mickco <chun20001015@hotmail.com>
2023-04-23 21:13:26 +0800

    Add: ErrorViewModel that listen and process errors  into snackbar

--- app/src/main/java/com/example/kcodetest/view/main/MainActivity.kt
+++ app/src/main/java/com/example/kcodetest/view/main/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.kcodetest.view.main
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.kcodetest.R
 import com.example.kcodetest.databinding.ActivityMainBinding
+import com.example.kcodetest.util.launchAndRepeatWithViewLifecycle
+import com.example.kcodetest.viewmodel.main.ErrorViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val errorViewModel: ErrorViewModel by viewModels()
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +26,19 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.mainActivityNavHostFragment)
         binding.mainActivityBottomNavView.setupWithNavController(navController)
+
+
+        launchAndRepeatWithViewLifecycle {
+            launch {
+                errorViewModel.displayErrorSnackbar.collect {
+                    Snackbar.make(
+                        binding.mainActivityCoordinatorLayout,
+                        it.message.getString(this@MainActivity),
+                        it.duration
+                    ).show()
+                }
+            }
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/kcodetest/viewmodel/main/ErrorViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.kcodetest.viewmodel.main
+
+import com.example.kcodetest.R
+import com.example.kcodetest.repository.model.ErrorCode
+import com.example.kcodetest.repository.model.KResult
+import com.example.kcodetest.viewmodel.common.BaseViewModel
+import com.example.kcodetest.viewmodel.common.ResourceWrap
+import com.example.kcodetest.viewmodel.common.TextWrap
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Named
+
+@HiltViewModel
+class ErrorViewModel @Inject constructor(
+    @Named("errorDisplayRequest") private val _errorDisplayRequest: MutableSharedFlow<KResult.Fail>
+) : BaseViewModel() {
+
+
+    val displayErrorSnackbar: Flow<KSnackBarUI> = _errorDisplayRequest.map {
+        when (it.errorCode) {
+            ErrorCode.ConnectionError,
+            ErrorCode.HTTPError -> KSnackBarUI(
+                message = ResourceWrap(R.string.network_error_message),
+                duration = Snackbar.LENGTH_LONG
+            )
+
+            else -> KSnackBarUI(
+                message = ResourceWrap(R.string.unknown_error_message),
+                duration = Snackbar.LENGTH_LONG
+            )
+        }
+    }
+}
+
+data class KSnackBarUI(
+    val message: TextWrap,
+    val duration: Int
+)
