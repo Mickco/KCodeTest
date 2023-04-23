7ba150e3d089d3fd4d4407387495ca7257c12b00
Mickco <chun20001015@hotmail.com>
2023-04-23 21:23:38 +0800

    Add: BookmarkFragment implementation

--- app/src/main/java/com/example/kcodetest/view/bookmark/BookmarkFragment.kt
+++ app/src/main/java/com/example/kcodetest/view/bookmark/BookmarkFragment.kt
@@ -5,17 +5,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.SimpleItemAnimator
 import com.example.kcodetest.R
+import com.example.kcodetest.databinding.FragmentBookmarkBinding
+import com.example.kcodetest.util.launchAndRepeatWithViewLifecycle
+import com.example.kcodetest.view.common.AlbumRecyclerViewAdapter
+import com.example.kcodetest.viewmodel.album.AlbumViewModel
+import kotlinx.coroutines.launch
 
 
 class BookmarkFragment : Fragment() {
+
+    private lateinit var binding: FragmentBookmarkBinding
+    private val viewModel: AlbumViewModel by navGraphViewModels(R.id.main_nav_graph) { defaultViewModelProviderFactory }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bookmark, container, false)
+        binding = FragmentBookmarkBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            val adapter = AlbumRecyclerViewAdapter(viewModel::onBookmarkClicked)
+            bookmarkFragmentRecyclerView.apply {
+                layoutManager = GridLayoutManager(context, NUM_COLUMN_COUNT)
+                this.adapter = adapter
+                (itemAnimator as SimpleItemAnimator).supportsChangeAnimations = false
+            }
+
+            bookmarkFragmentPullToRefresh.setOnRefreshListener {
+                viewModel.loadNewAlbumList()
+            }
+
+            launchAndRepeatWithViewLifecycle {
+                launch {
+                    viewModel.bookmarkedAlbumListUI.collect {
+                        adapter.uiList = it
+                    }
+                }
+                launch {
+                    viewModel.isLoading.collect {
+                        bookmarkFragmentPullToRefresh.isRefreshing = it
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val NUM_COLUMN_COUNT = 3
+    }
 }
